166cb7e32c7456a7d18507fca197e47185b00c50
ykmxxi <ykmxxi97@gmail.com>
2023-02-15 12:55:11 +0900

    Implement #1- WebApplication Initializer

--- src/main/java/com/example/config/Config.java
+++ src/main/java/com/example/config/Config.java
@@ -2,7 +2,9 @@ package com.example.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.example")
 @Configuration
 public class Config {
